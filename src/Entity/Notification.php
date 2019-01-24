d870a7683c378514b2d14f47f3babbebb7380d35
Yevhen Malykhin <yevhen.malykhin@cloudbeds.com>
2019-01-24 19:25:50 +0200

    recreate notification table

--- src/Entity/Notification.php
+++ src/Entity/Notification.php
@@ -3,9 +3,24 @@
 namespace App\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
+use Doctrine\ORM\Mapping\Entity;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\NotificationRepository")
+ * @ORM\Table(
+ *     name="notification",
+ *     uniqueConstraints={
+ *        @ORM\UniqueConstraint(
+ *            name="device_date_start_date_end_udx",
+ *            columns={"device", "date_start", "date_end"}
+ *        )
+ *     },
+ *     indexes={
+ *         @ORM\Index(name="device_date_start_idx", columns={"device", "date_start"}),
+ *         @ORM\Index(name="date_end_idx", columns={"date_end"}),
+ *     }
+ * )
+ * @Entity
  */
 class Notification
 {
--- src/Migrations/Version20190124172505.php
+++ src/Migrations/Version20190124172505.php
@@ -7,22 +7,30 @@ namespace DoctrineMigrations;
 use Doctrine\DBAL\Schema\Schema;
 use Doctrine\Migrations\AbstractMigration;
 
-final class Version20190123173022 extends AbstractMigration
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20190124172505 extends AbstractMigration
 {
     public function getDescription() : string
     {
-        return 'Create notification table';
+        return '';
     }
 
     public function up(Schema $schema) : void
     {
+        // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'sqlite', 'Migration can only be executed safely on \'sqlite\'.');
 
         $this->addSql('CREATE TABLE notification (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, device VARCHAR(255) NOT NULL, date_start DATETIME NOT NULL, date_end DATETIME DEFAULT NULL)');
+        $this->addSql('CREATE INDEX device_date_start_idx ON notification (device, date_start)');
+        $this->addSql('CREATE INDEX date_end_idx ON notification (date_end)');
+        $this->addSql('CREATE UNIQUE INDEX device_date_start_date_end_udx ON notification (device, date_start, date_end)');
     }
 
     public function down(Schema $schema) : void
     {
+        // this down() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'sqlite', 'Migration can only be executed safely on \'sqlite\'.');
 
         $this->addSql('DROP TABLE notification');
